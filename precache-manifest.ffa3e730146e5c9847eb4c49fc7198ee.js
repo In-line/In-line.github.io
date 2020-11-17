5eaefcbc4e14361085eb923888f76196c7fad42a
Alik Aslanyan <inline0@protonmail.com>
2020-11-17 22:22:45 +0400

    deploy

---
+++ precache-manifest.ffa3e730146e5c9847eb4c49fc7198ee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fcc5406d65e4ff00009",
+    "url": "/css/app.6ff98114.css"
+  },
+  {
+    "revision": "08328fd028a7def8e33160448fff5b69",
+    "url": "/fonts/BONX-AltMedium.08328fd0.otf"
+  },
+  {
+    "revision": "9123fdfecc25a6d5f6e53559996d93fb",
+    "url": "/fonts/BONX-Frame.9123fdfe.otf"
+  },
+  {
+    "revision": "b44b5a0e9666108c44f89008de057312",
+    "url": "/fonts/BONX-Medium.b44b5a0e.otf"
+  },
+  {
+    "revision": "0dcbf995cce64fc176fb3c421be7782a",
+    "url": "/fonts/BONX-Silhouette.0dcbf995.otf"
+  },
+  {
+    "revision": "7da8452ef5b29ca94ecb773e3986a2f0",
+    "url": "/fonts/BONX-TubeMedium.7da8452e.otf"
+  },
+  {
+    "revision": "20d02a29b6242507ab09c42f144bccd9",
+    "url": "/img/empty.20d02a29.png"
+  },
+  {
+    "revision": "b41022f07281728b911b93c05e6c931b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fcc5406d65e4ff00009",
+    "url": "/js/app.0b0412f8.js"
+  },
+  {
+    "revision": "f5696195d135da8a2e11",
+    "url": "/js/chunk-vendors.fd2df606.js"
+  },
+  {
+    "revision": "0a50e96602631186cfeefe13a52fea5d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
